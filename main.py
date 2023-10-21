763bc0f0d16c662b731f6a96e34fca233af1a23a
Athos Ishikawa <athos.iskw@gmail.com>
2023-10-20 21:40:16 -0300

    vr 3.0

--- main.py
+++ main.py
@@ -7,46 +7,43 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from model import Model
 from kivy.clock import Clock
+from kivy.uix.spinner import Spinner
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+import threading
 
 
-class CrudApp(App):
-    def __init__(self, model):
+
+class App(App):
+    def __init__(self):
         super().__init__()
-        self.model = model
+        self.model = None
         self.layout_atual = self.build() 
         Window.size = (300, 600)
-
-        
+        self.process_update_event = None
     
 
     def build(self):
-        # Cria o layout da tela principal.
+        self.model = Model(db_name="my_database", collection_name="my_collection")
+
         self.layoutLogin = BoxLayout(orientation='vertical', spacing=10, padding=20)
 
 
         label_login = Label(text='LOGIN', size_hint=(1, 0.1), height=50)
-        label_login.font_size = '24sp'  # Aumenta o tamanho da fonte
+        label_login.font_size = '24sp'  
 
-        # Cria um rótulo para o campo de e-mail.
         label_email = Label(text='E-mail:', size_hint=(1, 0.05), height=10)
         label_email.font_size = '20sp'
-
-        # Cria um widget TextInput para o campo de e-mail.
         self.input_email = TextInput(font_size='18sp', size_hint=(1, None), height=30, multiline=False)
 
-        # Cria um rótulo para o campo de senha.
         label_senha = Label(text='Senha:', size_hint=(1, 0.05), height=10)
         label_senha.font_size = '20sp'
-
-        # Cria um widget TextInput para o campo de senha.
         self.input_senha = TextInput(size_hint=(1, None), height=30, multiline=False, password=True)
 
-        # Cria um botão para fazer login.
         button_login = Button(text='Login', size_hint=(1, None), height=50)
         button_login.bind(on_press=self.login)
         self.message_label = Label(text="", size_hint=(1, 0.1), height=30, color=(1, 0, 0, 1), font_size='16sp')
 
-        # Adiciona os widgets ao layout interno.
         self.layoutLogin.add_widget(label_login)
         self.layoutLogin.add_widget(label_email)
         self.layoutLogin.add_widget(self.input_email)
@@ -55,21 +52,16 @@ class CrudApp(App):
         self.layoutLogin.add_widget(button_login)
         self.layoutLogin.add_widget(self.message_label)
 
-        # Define a imagem de fundo do layout.
         return self.layoutLogin
 
 
 
-
     def build2(self):
-        # Define o tamanho da tela
-
         self.layout = BoxLayout(orientation='vertical')
 
         size = (1, 0.1)
 
 
-        # Cria os rótulos
         label_titulo = Label(text='CRUD', size_hint=size)
         label_pid = Label(text='Nome do Processo (PID)', size_hint=size)
         label_uid = Label(text='Nome do Usuário (UID)', size_hint=size)
@@ -79,69 +71,71 @@ class CrudApp(App):
         label_memoria = Label(text='Espaço de Memória (mb)', size_hint=size)
         self.message_label = Label(text="", size_hint=size, color=(1, 0, 0, 1))
 
-        # Cria os campos de entrada
         self.pid_entry = TextInput(size_hint=size)
         self.uid_entry = TextInput(size_hint=size)
-        self.prioridade_entry = TextInput(size_hint=size)
+        self.prioridade_spinner = Spinner(
+            text='Selecione a prioridade',
+            size_hint=size,
+            values=('Alta', 'Média', 'Baixa')
+        )
         self.cpu_entry = TextInput(size_hint=size)
-        self.estado_entry = TextInput(size_hint=size)
-        self.memoria_entry = TextInput(size_hint=size)  # Instancia um novo objeto TextInput
+        self.estado_spinner = Spinner(
+            text='Selecione o estado',
+            size_hint=size,
+            values=('Pronto', 'Execução', 'Espera')
+        )
+        self.memoria_entry = TextInput(size_hint=size)  
 
-        # Cria os botões
+        # Botões
         salvar_button = Button(text='Salvar', size_hint=size)
-        #cancelar_button = Button(text='Cancelar', size_hint=size)
         consultar_button = Button(text='Consultar', size_hint=size)
         atualizar_button = Button(text='Atualizar', size_hint=size)
         deletar_button = Button(text='Deletar', size_hint=size)
+        processos_button = Button(text='Processos', size_hint=size)
 
-        #Adiciona a função aos botões
         salvar_button.on_press = self.salvar
 
         consultar_button.on_press = self.consultar
         atualizar_button.on_press = self.atualizar
         deletar_button.on_press = self.deletar
+        processos_button.on_press = self.build3
 
-        # Adiciona os rótulos e campos de entrada ao layout
         self.layout.add_widget(label_titulo)
         self.layout.add_widget(label_pid)
         self.layout.add_widget(self.pid_entry)
         self.layout.add_widget(label_uid)
         self.layout.add_widget(self.uid_entry)
         self.layout.add_widget(label_prioridade)
-        self.layout.add_widget(self.prioridade_entry)
+        self.layout.add_widget(self.prioridade_spinner)
         self.layout.add_widget(label_cpu)
         self.layout.add_widget(self.cpu_entry)
         self.layout.add_widget(label_estado)
-        self.layout.add_widget(self.estado_entry)
+        self.layout.add_widget(self.estado_spinner)
         self.layout.add_widget(label_memoria)
         self.layout.add_widget(self.memoria_entry)
 
 
-        # Adiciona os botões ao layout
         self.layout.add_widget(salvar_button)
         self.layout.add_widget(consultar_button)
         self.layout.add_widget(atualizar_button)
         self.layout.add_widget(deletar_button)
-        #self.layout.add_widget(cancelar_button)
+        self.layout.add_widget(processos_button)
         self.layout.add_widget(self.message_label)
 
         return self.layout
 
     def salvar(self):
-        # Obtém os dados do usuário
         pid = self.pid_entry.text
         uid = self.uid_entry.text
-        prioridade = self.prioridade_entry.text
+        prioridade = self.prioridade_spinner.text
         cpu = self.cpu_entry.text
-        estado = self.estado_entry.text
+        estado = self.estado_spinner.text
         memoria = self.memoria_entry.text
 
-        # Verifica se algum dos campos está vazio
         if not pid or not uid or not prioridade or not cpu or not estado or not memoria:
             self.mostrar_mensagem("Preencha todos os campos.")
             return
         
-        # Cria um documento para salvar no banco de dados
         document = {
             "pid": pid,
             "uid": uid,
@@ -161,26 +155,22 @@ class CrudApp(App):
 
     def consultar(self):
         try:
-            # Obtém o PID do processo a ser consultado
             pid = self.pid_entry.text
 
-            # Verifica se o campo PID não está vazio
             if not pid:
                 raise ValueError("Digite um PID para consultar.")
 
-            # Consulta o documento no banco de dados
             documents = list(self.model.find_documents({"pid": pid}))
 
             if not documents:
                 raise ValueError("Processo não encontrado.")
 
-            # Atualiza os campos de entrada com os dados do primeiro documento encontrado
             document = documents[0]
             self.pid_entry.text = document["pid"]
             self.uid_entry.text = document["uid"]
-            self.prioridade_entry.text = document["prioridade"]
+            self.prioridade_spinner.text = document["prioridade"]
             self.cpu_entry.text = document["cpu"]
-            self.estado_entry.text = document["estado"]
+            self.estado_spinner.text = document["estado"]
             self.memoria_entry.text = document["memoria"]
 
         except ValueError as e:
@@ -188,23 +178,20 @@ class CrudApp(App):
 
 
     def atualizar(self):
-        # Obtém os dados do usuário
+        #Ddados do usuário
         pid = self.pid_entry.text
         uid = self.uid_entry.text
-        prioridade = self.prioridade_entry.text
+        prioridade = self.prioridade_spinner.text
         cpu = self.cpu_entry.text
-        estado = self.estado_entry.text
+        estado = self.estado_spinner.text
         memoria = self.memoria_entry.text
 
-        # Verifica se algum dos campos está vazio
         if not pid or not uid or not prioridade or not cpu or not estado or not memoria:
             self.mostrar_mensagem("Preencha todos os campos.")
             return
 
-        # Cria um objeto query para atualizar o documento
         query = {"pid": pid}
 
-        # Cria um objeto update para definir os novos valores do documento
         update = {
             "$set": {
                 "pid": pid,
@@ -225,15 +212,12 @@ class CrudApp(App):
 
 
     def deletar(self):
-        # Obtém o PID do processo a ser deletado
         pid = self.pid_entry.text
 
-        # Verifica se o campo PID não está vazio
         if not pid:
             self.mostrar_mensagem("Digite um PID para deletar.")
             return
 
-        # Deleta o documento no banco de dados
         self.model.delete_document({"pid": pid})
 
         self.limpar_campos()
@@ -243,54 +227,148 @@ class CrudApp(App):
     def limpar_campos(self):
         self.pid_entry.text = ""
         self.uid_entry.text = ""
-        self.prioridade_entry.text = ""
+        self.prioridade_spinner.text = ""
         self.cpu_entry.text = ""
-        self.estado_entry.text = ""
+        self.estado_spinner.text = ""
         self.memoria_entry.text = ""
 
 
     def mostrar_mensagem(self, mensagem):
-        # Atualiza o texto do widget Label para exibir a nova mensagem
         self.message_label.text = mensagem
-
-        # Define um temporizador para limpar a mensagem após alguns segundos
-        Clock.schedule_once(self.limpar_mensagem, 3)  # Limpa a mensagem após 3 segundos
+        Clock.schedule_once(self.limpar_mensagem, 3)  
     
     def limpar_mensagem(self, dt):
-        # Limpa o texto do widget Label
         self.message_label.text = ""
     
     def login(self, button):
-        # Obtém os valores dos campos de e-mail e senha.
         email = self.input_email.text
         senha = self.input_senha.text
 
-        # Valida os valores dos campos.
         if email == '' or senha == '':
             self.mostrar_mensagem('Preencha todos os campos.')
             return
 
         # Faz a autenticação do usuário.
-        # (Esse código é apenas um exemplo, você deve implementar seu próprio código de autenticação.)
         if email == 'admin' and senha == '123456':
             self.mostrar_mensagem('Login efetuado com sucesso!')
             self.layoutLogin.clear_widgets()
 
-            # Defina o layout atual como o layout principal
             self.layout_atual = self.build2()
-
-            # Adicione o layout atual ao aplicativo
             self.root.add_widget(self.layout_atual)
         else:
             self.mostrar_mensagem('E-mail ou senha inválidos.')
+
+    def show_records(self):
+
+
+        self.layout.clear_widgets()
+        self.layout_atual = self.buildProcessos()
+        self.root.add_widget(self.layout_atual)
+
+    def build3(self):
+        self.model.cria_registros()
+        Window.size = (1200, 700)
+
+        self.layoutProcessos2 = BoxLayout(orientation='vertical')
+
+        # Recupere os registros do banco de dados
+        records = self.model.show_records()
         
+        # Crie uma lista de Labels para exibir os registros
+        processo_labels = []
+            
+        for record in records:
+            processo_label = Label(text=f'PID: {record["pid"]}, UID: {record["uid"]}, Prioridade: {record["prioridade"]}, '
+                                        f'CPU: {record["cpu"]}, Estado: {record["estado"]}, Memória: {record["memoria"]}')
+            processo_labels.append(processo_label)  # Adicione o Label do processo à lista
+
+        for processo_label in processo_labels:
+            self.layoutProcessos2.add_widget(processo_label)  # Adicione os Labels ao layout vertical
+
+        if not self.process_update_event:
+            self.process_update_event = Clock.schedule_interval(self.update_process_states, 2)
+
+
+
+
+        self.layout.clear_widgets()
+        self.layout_atual = self.layoutProcessos2
+        self.root.add_widget(self.layout_atual)
+
+        return self.layoutProcessos2
+    
+    def update_process_states(self, dt):
+        # Verifica se a execução anterior da função ainda está em andamento
+        if not getattr(self, 'updating_process_states', False):
+            # Inicia a atualização em um thread separado
+            self.updating_process_states = True
+            threading.Thread(target=self.update_process_states_thread).start()
+
+    def update_process_states_thread(self):
+        try:
+            self.model.mudar_estado()
+            # Após atualizar o estado dos processos, atualize os textos dos Labels
+            records = self.model.show_records()
+            for i, processo_label in enumerate(self.layoutProcessos2.children):
+                processo_label.text = f'PID: {records[i]["pid"]}, UID: {records[i]["uid"]}, ' \
+                                    f'Prioridade: {records[i]["prioridade"]}, ' \
+                                    f'CPU: {records[i]["cpu"]}, ' \
+                                    f'Estado: {records[i]["estado"]}, ' \
+                                    f'Memória: {records[i]["memoria"]}'
+        finally:
+            # Marca que a atualização terminou
+            self.updating_process_states = False
+    
+    def atualizar_labels(self):
+        records = self.model.show_records()
+        for label in self.layoutProcessos2.children:
+            if label.text == 'Estado':
+                try:
+                    pid = int(label.id)
+                    if pid <= len(records):
+                        label.text = records[pid - 1]['estado']
+                except ValueError:
+                    pass
+                except IndexError:
+                    pass    
+
+
+    # def buildProcessos(self):
+    #     self.layoutProcessos = BoxLayout(orientation='horizontal')
+    #     Window.size = (1200, 700)
+
+    #     records = self.model.show_records()
+    #     column_titles = ["ID", "PID", "UID", "PRIORIDADE", "CPU", "ESTADO", "MEMÓRIA"]  
+
+    #     grid_layout = GridLayout(cols=len(column_titles), spacing=(0, 5))  
+
+    #     for title in column_titles:
+    #         grid_layout.add_widget(Label(text=title, bold=True))
+
+    #     for record in records:
+    #         for field in record.keys():
+    #             grid_layout.add_widget(Label(text=str(record[field])))
+
+    #     scroll_view = ScrollView()
+    #     scroll_view.add_widget(grid_layout)
+
+    #     self.layoutProcessos.add_widget(scroll_view)
+
+    #     return self.layoutProcessos
 
     def run(self):
         self.build()
         super().run()
 
+    def on_stop(self):
+        if self.process_update_event:
+            self.process_update_event.cancel()
+
+        if self.model:
+            # Certifique-se de liberar a conexão do MongoDB
+            self.model.client.close()
+
 if __name__ == '__main__':
 
-    model = Model(db_name="my_database", collection_name="my_collection")
-    crud_app = CrudApp(model)
+    crud_app = App()
     crud_app.run()
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import pymongo
-
+import random
+import threading
+import time
 
 class Model:
     def __init__(self, db_name, collection_name):
@@ -7,6 +9,10 @@ class Model:
         self.client = pymongo.MongoClient("mongodb://localhost:27017")
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
+        self.lock = threading.Lock()  # Semáforo para proteger operações no MongoDB
+        # Deleta todos os registros ao inicializar o modelo
+        self.collection.delete_many({}) 
+
 
     def insert_document(self, document):
         # Insere um documento no banco de dados
@@ -18,9 +24,69 @@ class Model:
 
     def update_document(self, query, update):
         # Atualiza um documento no banco de dados
-        self.collection.update_one(query, update)
+        with self.lock:
+            self.collection.update_one(query, update)
 
     def delete_document(self, query):
         # Exclui um documento do banco de dados
         self.collection.delete_one(query)
 
+    def cria_registros(self):
+        for _ in range(5):
+            pid = random.randint(1, 100)
+            uid = random.randint(1, 100)
+            prioridade = random.choice(['Alta', 'Média', 'Baixa'])
+            cpu = random.randint(1, 100)
+            estado = 'Início'
+            memoria = random.randint(1, 100)
+
+            registro = {
+                'pid': pid,
+                'uid': uid,
+                'prioridade': prioridade,
+                'cpu': cpu,
+                'estado': estado,
+                'memoria': memoria,
+            }
+
+            # Insira o registro no banco de dados
+            self.insert_document(registro)
+
+    def mudar_estado(self):
+        # Obtenha todos os processos em estado "Início"
+        processos_inicio = list(self.collection.find({"estado": "Início"}))
+
+        if processos_inicio:
+            for processo in processos_inicio:
+                processo['estado'] = 'Pronto'
+                self.update_document({'pid': processo['pid']}, {'$set': {'estado': 'Pronto'}})
+        else:
+            # Obtenha o processo em estado "Execução"
+            processo_execucao = self.collection.find_one({"estado": "Execução"})
+
+            if processo_execucao:
+                # Use random.choice para escolher aleatoriamente o próximo estado
+                proximo_estado = random.choice(["Pronto", "Término", "Espera"])
+                processo_execucao['estado'] = proximo_estado
+                self.update_document({'pid': processo_execucao['pid']}, {'$set': {'estado': proximo_estado}})
+            else:
+                # Obtenha o processo em estado "Pronto"
+                processo_pronto = self.collection.find_one({"estado": "Pronto"})
+
+                if processo_pronto:
+                    processo_pronto['estado'] = 'Execução'
+                    self.update_document({'pid': processo_pronto['pid']}, {'$set': {'estado': 'Execução'}})
+                else:
+                    # Obtenha o processo em estado "Espera"
+                    processo_espera = self.collection.find_one({"estado": "Espera"})
+
+                    if processo_espera:
+                        processo_espera['estado'] = 'Pronto'
+                        self.update_document({'pid': processo_espera['pid']}, {'$set': {'estado': 'Pronto'}})
+
+        return
+
+
+    def show_records(self):
+        records = self.collection.find()
+        return records
